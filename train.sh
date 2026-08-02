65ae12a339cbe6011eee1c550de515432e667316
kschmid <kschmid@nvidia.com>
2026-08-02 19:29:56 +1000

    check

--- train.sh
+++ train.sh
@@ -107,6 +107,7 @@ exec $RUN python scripts/train_world_model.py \
     model.architecture.config.codec_checkpoint="$CODEC" \
     "${idx[@]}" \
     run.batch_size=1 run.compile=false wandb.mode=disabled dataloader.num_workers="$WORKERS" run.log_every=50 validation.downstream_val_every=7000 run.checkpoint_every=7000 run.checkpoint_keep_recent=2 \
+    world_model_metrics.num_samples=32 world_model_metrics.dino_max_chunk_size=32 \
     run.output_dir="$output_dir" \
     $continue_from \
     '++tensorboard.logdir=${run.output_dir}/tb' "$@"
